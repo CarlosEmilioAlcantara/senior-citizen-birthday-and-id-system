b360c7e63d4095ea2753a6daa20c6ac25b609105
CarlosEmilioAlcantara <agq.xyz@gmail.com>
2025-12-19 15:19:48 +0800

    Admin dashboard

--- server/app/routes/routes.py
+++ server/app/routes/routes.py
@@ -1,3 +1,4 @@
+from app.routes.blueprints.admin_routes import admin_bp
 from app.routes.blueprints.admins_routes import admins_bp
 from app.routes.blueprints.auth_routes import auth_bp
 from app.routes.blueprints.error_routes import errors_bp
@@ -10,5 +11,6 @@ def register_routes(app):
     app.register_blueprint(errors_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(users_bp)
+    app.register_blueprint(admin_bp)
     app.register_blueprint(admins_bp)
     app.register_blueprint(superadmins_bp)
