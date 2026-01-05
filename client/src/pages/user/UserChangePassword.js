dc15f471d0d5ff814be003e465335a69aee28206
trisssh <samillanotrisha768@gmail.com>
2026-01-05 12:20:50 +0800

    AddSideBarforUserChangePass

--- client/src/pages/user/UserChangePassword.js
+++ client/src/pages/user/UserChangePassword.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useCsrfToken from "../CsrfToken";
+import Sidebar from "../../components/Sidebar";
 
 export default function UserChangePassword() {
   const [status, setStatus] = useState("");
@@ -19,9 +20,9 @@ export default function UserChangePassword() {
         method: "POST",
         credentials: "include",
         body: fd,
-      })
+      });
       const data = await res.json();
-      setErrors({...data.errors});
+      setErrors({ ...data.errors });
 
       if (data.status === 429) {
         navigate("/too-many-requests");
@@ -39,50 +40,51 @@ export default function UserChangePassword() {
     }
   }
 
+  // SideBar
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    <div>
-      { !status && (
-        <p style={{color: "red"}}>{response}</p>
-      )}
+    <div className="flex bg-white md:h-screen">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-      <form onSubmit={handleChangePassword}>
-        <h3>User Change Password</h3>
-        <label>Old Password</label>
-        <input
-          type="password"
-          placeholder="password"
-          name="old_password"
-        />
-        <br/>
-        <small style={{"color": "red"}}>{ errors.old_password}</small>
-        <br />
+      {/* MAIN CONTENT AREA */}
+      <div className="flex-1 flex flex-col ml-0 lg:ml-auto h-screen">
+        {/* HEADER */}
+      
 
-        <label>New Password</label>
-        <input
-          type="password"
-          placeholder="password"
-          name="password"
-        />
-        <br/>
-        <small style={{"color": "red"}}>{ errors.password}</small>
-        <br />
+        {/* SCROLLABLE  USER CHANGE PASSWORD CONTENT */}
+        <main className="flex-1 overflow-y-auto p-5 bg-white">
+          {!status && <p style={{ color: "red" }}>{response}</p>}
 
-        <label>Confirm New Password</label>
-        <input
-          type="password"
-          placeholder="password"
-          name="confirm"
-        />
-        <br/>
-        <small style={{"color": "red"}}>{ errors.confirm}</small>
-        <br />
+          <form onSubmit={handleChangePassword}>
+            <h3>User Change Password</h3>
+            <label>Old Password</label>
+            <input type="password" placeholder="password" name="old_password" />
+            <br />
+            <small style={{ color: "red" }}>{errors.old_password}</small>
+            <br />
 
-        <button type="submit">Change Password</button>
-      </form>
+            <label>New Password</label>
+            <input type="password" placeholder="password" name="password" />
+            <br />
+            <small style={{ color: "red" }}>{errors.password}</small>
+            <br />
 
-      <nav>
-        <button><Link to="/">Cancel</Link></button>
-      </nav>
+            <label>Confirm New Password</label>
+            <input type="password" placeholder="password" name="confirm" />
+            <br />
+            <small style={{ color: "red" }}>{errors.confirm}</small>
+            <br />
+
+            <button type="submit">Change Password</button>
+          </form>
+
+          <nav>
+            <button>
+              <Link to="/">Cancel</Link>
+            </button>
+          </nav>
+        </main>
+      </div>
     </div>
   );
 }
