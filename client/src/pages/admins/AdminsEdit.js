ed669dab3cb6b538e7dc8ce07dae6b3f928a4f01
trisssh <samillanotrisha768@gmail.com>
2026-01-19 15:39:51 +0800

    `Added Sidebar component and refactored AdminsEdit page layout`

--- client/src/pages/admins/AdminsEdit.js
+++ client/src/pages/admins/AdminsEdit.js
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useCsrfToken from "../CsrfToken";
+import Sidebar from "../../components/Sidebar";
 
 export default function AdminsEdit() {
+  const csrfToken = useCsrfToken();
+  const navigate = useNavigate();
+
+  // --------------------------
+  // State
+  // --------------------------
   const [role, setRole] = useState("");
   const [status, setStatus] = useState("");
   const [response, setResponse] = useState("");
@@ -10,17 +17,16 @@ export default function AdminsEdit() {
   const [username, setUsername] = useState("");
   const [info, setInfo] = useState({});
   const [errors, setErrors] = useState({});
-  const csrfToken = useCsrfToken();
-  const navigate = useNavigate();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   async function getAdminInfo() {
     try {
       const res = await fetch("/admins/info", {
         method: "GET",
         credentials: "include",
-      })
+      });
       const data = await res.json();
-      setInfo({...data.info});
+      setInfo({ ...data.info });
 
       if (!data.success) {
         setStatus(data.success);
@@ -39,7 +45,7 @@ export default function AdminsEdit() {
       const res = await fetch("/auth/get-role", {
         method: "GET",
         credentials: "include",
-      }) 
+      });
       const data = await res.json();
       setRole(data.role);
     } catch (err) {
@@ -50,26 +56,26 @@ export default function AdminsEdit() {
   useEffect(() => {
     setEmail(info.email);
     setUsername(info.username);
-  }, [info])
+  }, [info]);
 
   useEffect(() => {
     getRole();
-    getAdminInfo()
-  }, [])
+    getAdminInfo();
+  }, []);
 
   async function handleAdminEdit(e) {
     e.preventDefault();
     const fd = new FormData(e.target);
-    fd.set("csrf_token", csrfToken)
+    fd.set("csrf_token", csrfToken);
 
     try {
       const res = await fetch("/admins/edit", {
         method: "POST",
         credentials: "include",
         body: fd,
-      })
+      });
       const data = await res.json();
-      setErrors({...data.errors});
+      setErrors({ ...data.errors });
 
       if (!data.success) {
         setStatus(data.success);
@@ -86,68 +92,100 @@ export default function AdminsEdit() {
     }
   }
 
-  return(
-    <div>
-      { !status && (
-        <p style={{color: "red"}}>{response}</p>
-      )}
+  return (
+    <div className="flex bg-white min-h-screen">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-      <h3>Superadmin Edit Account</h3>
-      <form onSubmit={handleAdminEdit}>
-        <label>Email</label>
-        <input
-          type="email"
-          placeholder="email@email.com"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br/>
-        <small style={{"color": "red"}}>{errors.email}</small>
-        <br/>
+      {/* MAIN CONTENT AREA */}
+      <div className="flex-1 flex flex-col ml-0 lg:ml-auto h-screen">
+        {/* HEADER */}
+        <header className="bg-white flex items-center p-4 drop-shadow-[0_0_0.25rem_#0097A7]">
+          <button
+            onClick={() => setSidebarOpen(true)}
+            className="cursor-pointer p-2 hover:text-cyan-700 hover:scale-110 lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
 
-        <label>Username</label>
-        <input
-          type="text"
-          placeholder="username"
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br/>
-        <small style={{"color": "red"}}>{errors.username}</small>
-        <br/>
+          <div>
+            <h1 className="text-2xl font-bold">Edit Account</h1>
+          </div>
+        </header>
 
-        <label>Password</label>
-        <input
-          type="password"
-          placeholder="password"
-          name="password"
-        />
-        <br/>
-        <small>Leave empty to not change</small>
-        <small style={{"color": "red"}}>{errors.password}</small>
-        <br/>
+        <main className="flex-1 overflow-y-auto p-5 bg-white">
+          {!status && <p style={{ color: "red" }}>{response}</p>}
 
-        <label>Confirm Password</label>
-        <input
-          type="password"
-          placeholder="confirm password"
-          name="confirm"
-        />
-        <br/>
-        <small style={{"color": "red"}}>{errors.confirm}</small>
-        <br/>
+          {/* <h3>Superadmin Edit Account</h3> */}
+          <form onSubmit={handleAdminEdit}>
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="email@email.com"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <small style={{ color: "red" }}>{errors.email}</small>
+            <br />
 
-        <button type="submit">Edit Account</button>
-      </form>
+            <label>Username</label>
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <br />
+            <small style={{ color: "red" }}>{errors.username}</small>
+            <br />
 
-      { role === "admin" && (
-        <button><Link to="/admin-dashboard">Cancel</Link></button>
-      )}
-      { role === "superadmin" && (
-        <button><Link to="/superadmin-dashboard">Cancel</Link></button>
-      )}
+            <label>Password</label>
+            <input type="password" placeholder="password" name="password" />
+            <br />
+            <small>Leave empty to not change</small>
+            <small style={{ color: "red" }}>{errors.password}</small>
+            <br />
+
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              placeholder="confirm password"
+              name="confirm"
+            />
+            <br />
+            <small style={{ color: "red" }}>{errors.confirm}</small>
+            <br />
+
+            <button type="submit">Edit Account</button>
+          </form>
+
+          {role === "admin" && (
+            <button>
+              <Link to="/admin-dashboard">Cancel</Link>
+            </button>
+          )}
+          {role === "superadmin" && (
+            <button>
+              <Link to="/superadmin-dashboard">Cancel</Link>
+            </button>
+          )}
+        </main>
+      </div>
     </div>
   );
 }
