ba6248ed408ff27bc9a5bcf2757361c0b8c13b28
trisssh <samillanotrisha768@gmail.com>
2026-01-24 15:30:39 +0800

    `Refactor SuperadminAdminsList.js: update delete confirmation popup`

--- client/src/pages/superadmin/SuperadminAdminsList.js
+++ client/src/pages/superadmin/SuperadminAdminsList.js
@@ -543,7 +543,6 @@ export default function SuperadminAdminsList() {
                   <form onSubmit={handleEditAdmin} className="bg-white">
                     {!status && <p className="text-red-500">{response}</p>}
 
-                    {/* <label>Editing Admin {id}</label> */}
                     <div className="grid grid-cols-1 gap-4 mb-4">
                       <div>
                         <label className="text-sm text-gray-600">
@@ -608,15 +607,68 @@ export default function SuperadminAdminsList() {
 
           {openDelete && (
             <div className="popup">
-              <form onSubmit={handleDeleteAdmin}>
-                <label>Delete Confirmation for Admin {id}</label>
-                <br />
-                <p>Are you sure you want to delete this admin?</p>
+              <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+                <div className="bg-white w-full max-w-md rounded-lg shadow-lg p-6 relative border border-red-500 ">
+                  {/* Close button */}
+                  <button
+                    onClick={() => setOpenDelete(false)}
+                    className="absolute top-3 right-3 text-gray-500 hover:text-gray-800"
+                  >
+                    ✕
+                  </button>
 
-                <button type="submit">Delete Admin</button>
-              </form>
+                  <form
+                    onSubmit={handleDeleteAdmin}
+                    className="flex flex-col gap-4 items-center"
+                  >
+                    <div className="flex justify-center sm:justify-start">
+                      <div className="size-28 rounded-full bg-red-600 flex items-center justify-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-20 text-white"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                          />
+                        </svg>
+                      </div>
+                    </div>
 
-              <button onClick={() => setOpenDelete(false)}>Cancel</button>
+                    <div className="flex flex-col">
+                      <p className="text-2xl text-center font-semibold text-gray-700">
+                        Are you sure you want to delete this Admin Account?
+                      </p>
+                      <p className="text-sm tracking-wider text-center font-semibold text-gray-500">
+                        This action will delete this entire account, including
+                        all saved data. This cannot be undone.
+                      </p>
+                    </div>
+                    {/* <label>Delete Confirmation for {id}</label> */}
+                    <div>
+                      <div className="flex justify-between items-center gap-4">
+                        <button
+                          className="px-6 md:px-8 py-2 rounded bg-gray-200 hover:bg-gray-300"
+                          onClick={() => setOpenDelete(false)}
+                        >
+                          No, cancel
+                        </button>
+                        <button
+                          className="px-10 md:px-12 py-2 rounded bg-red-700 hover:bg-red-800 text-white font-medium"
+                          type="submit"
+                        >
+                          Yes, Delete Account
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
           )}
 
