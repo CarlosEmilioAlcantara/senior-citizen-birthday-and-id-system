45bea6f945122a4b7fa1774c93933fe4496438b1
trisssh <samillanotrisha768@gmail.com>
2026-01-24 16:07:53 +0800

    `Refactored UsersList.js and added delete account confirmation popup`

--- client/src/pages/admins/UsersList.js
+++ client/src/pages/admins/UsersList.js
@@ -473,84 +473,82 @@ export default function UsersList() {
 
         {/* SCROLLABLE  ADMIN LIST ACC CONTENT */}
         <main className="flex-1  overflow-auto p-5 bg-white">
-          
-            {/* <h3>List of Senior Citizens</h3> */}
-            <div className="grid md:grid-cols-3 gap-2">
-              <form onSubmit={handleFetchUsers}>
-                <h3 className="font-semibold text-gray-500">Search</h3>
-                <div className="relative flex justify-between  py-0 gap-0 border border-gray-300 rounded-md focus:outline-blue-600">
-                  <div className="flex justify-center items-center  rounded focus:outline-blue-600 px-2">
-                    {/* SVG */}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                      />
-                    </svg>
-                    <input
-                      className="border-none outline-none w-full ps-2"
-                      // className="block w-full ps-1"
-                      type="text"
-                      placeholder="Search by Email or Name"
-                      value={emailOrFullname}
-                      // onKeyDown={(e) => {
-                      //   if (e.key === "Enter") {
-                      //     e.preventDefault();
-                      //   }}
-                      // }
-                      onChange={(e) => setEmailOrFullname(e.target.value)}
-                    />
-                  </div>
-
-                  <button
-                    type="submit"
-                    className="text-white bg-blue-600 hover:bg-blue-700 rounded-r py-1.5 px-6"
+          {/* <h3>List of Senior Citizens</h3> */}
+          <div className="grid md:grid-cols-3 gap-2">
+            <form onSubmit={handleFetchUsers}>
+              <h3 className="font-semibold text-gray-500">Search</h3>
+              <div className="relative flex justify-between  py-0 gap-0 border border-gray-300 rounded-md focus:outline-blue-600">
+                <div className="flex justify-center items-center  rounded focus:outline-blue-600 px-2">
+                  {/* SVG */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5"
                   >
-                    Search
-                  </button>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                    />
+                  </svg>
+                  <input
+                    className="border-none outline-none w-full ps-2"
+                    // className="block w-full ps-1"
+                    type="text"
+                    placeholder="Search by Email or Name"
+                    value={emailOrFullname}
+                    // onKeyDown={(e) => {
+                    //   if (e.key === "Enter") {
+                    //     e.preventDefault();
+                    //   }}
+                    // }
+                    onChange={(e) => setEmailOrFullname(e.target.value)}
+                  />
                 </div>
-              </form>
 
-              <div>
-                <p className="font-semibold text-gray-500">Filter By</p>
-                <select
-                  className="w-full border rounded px-3 py-1.5"
-                  value={filter}
-                  onChange={(e) => setFilter(e.target.value)}
+                <button
+                  type="submit"
+                  className="text-white bg-blue-600 hover:bg-blue-700 rounded-r py-1.5 px-6"
                 >
-                  <option value={"all"}>All</option>
-                  <option value={1}>Verified</option>
-                  <option value={0}>Unverified</option>
-                </select>
+                  Search
+                </button>
               </div>
+            </form>
 
-              <div>
-                <p className="font-semibold text-gray-500">Sort By</p>
-                <select
-                  className="w-full border rounded px-3 py-1.5"
-                  value={sort}
-                  onChange={(e) => setSort(e.target.value)}
-                >
-                  <option value={"Newest"}>Newest to Oldest</option>
-                  <option value={"Oldest"}>Oldest to Newest</option>
-                  <option value={"Updated"}>Last Updated</option>
-                  <option value={"Unupdated"}>Last Unupdated</option>
-                </select>
-              </div>
+            <div>
+              <p className="font-semibold text-gray-500">Filter By</p>
+              <select
+                className="w-full border rounded px-3 py-1.5"
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+              >
+                <option value={"all"}>All</option>
+                <option value={1}>Verified</option>
+                <option value={0}>Unverified</option>
+              </select>
             </div>
-          
+
+            <div>
+              <p className="font-semibold text-gray-500">Sort By</p>
+              <select
+                className="w-full border rounded px-3 py-1.5"
+                value={sort}
+                onChange={(e) => setSort(e.target.value)}
+              >
+                <option value={"Newest"}>Newest to Oldest</option>
+                <option value={"Oldest"}>Oldest to Newest</option>
+                <option value={"Updated"}>Last Updated</option>
+                <option value={"Unupdated"}>Last Unupdated</option>
+              </select>
+            </div>
+          </div>
 
           {/* TABLE CONTAINER */}
           <div className="mt-6 overflow-hidden">
-            <h3 className="text-xl font-bold text-gray-800 mb-4">
+            <h3 className="text-2xl font-bold text-gray-800 mb-4">
               Senior Citizen's Account List Table
             </h3>
             <div className="overflow-x-auto rounded-lg shadow-md ">
@@ -1049,13 +1047,68 @@ export default function UsersList() {
           )}
           {openDelete && (
             <div className="popup">
-              <form onSubmit={handleDeleteUser}>
-                <p style={{ color: "red" }}>
-                  This will permanently delete senior #{id}, are you sure?
-                </p>
-                <button type="submit">Confirm</button>
-              </form>
-              <button onClick={() => setOpenDelete(false)}>Cancel</button>
+              <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+                <div className="bg-white w-full max-w-md rounded-lg shadow-lg p-6 relative border border-red-500 ">
+                  {/* Close button */}
+                  <button
+                    onClick={() => setOpenDelete(false)}
+                    className="absolute top-3 right-3 text-gray-500 hover:text-gray-800"
+                  >
+                    ✕
+                  </button>
+
+                  <form
+                    onSubmit={handleDeleteUser}
+                    className="flex flex-col gap-4 items-center"
+                  >
+                    <div className="flex justify-center sm:justify-start">
+                      <div className="size-28 rounded-full bg-red-600 flex items-center justify-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-20 text-white"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+
+                    <div className="flex flex-col">
+                      <p className="text-2xl text-center font-semibold text-gray-700">
+                        Are you sure you want to delete this Senior Citizen's Account?
+                      </p>
+                      <p className="text-sm tracking-wider text-center font-semibold text-gray-500">
+                        This action will delete this entire account, including
+                        all saved data. This cannot be undone.
+                      </p>
+                    </div>
+
+                    <div>
+                      <div className="flex justify-between items-center gap-4">
+                        <button
+                          className="px-6 md:px-8 py-2 rounded bg-gray-200 hover:bg-gray-300"
+                          onClick={() => setOpenDelete(false)}
+                        >
+                          No, cancel
+                        </button>
+                        <button
+                          className="px-10 md:px-12 py-2 rounded bg-red-700 hover:bg-red-800 text-white font-medium"
+                          type="submit"
+                        >
+                          Yes, Delete Account
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
           )}
           {openCard && (
