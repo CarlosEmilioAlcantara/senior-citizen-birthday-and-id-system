26ddb09b43ca3da8fa88875e9217f09702b9b441
trisssh <samillanotrisha768@gmail.com>
2026-01-28 13:41:31 +0800

    `Updated RED DESIGN UserLogin.js`

--- client/src/pages/user/UserLogin.js
+++ client/src/pages/user/UserLogin.js
@@ -80,18 +80,116 @@ export default function UserLogin() {
   }
 
   return (
+    // ---------------------------
+    // BLUE LOGIN
+    // ---------------------------
+    // <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen bg-white">
+    //   {/* LEFT SIDE */}
+    //   <div className="bg-gradient-to-b from-cyan-700 to-blue-700 flex flex-col justify-center items-center text-center p-10 border-8 border-white rounded-2xl">
+    //     <img src="logo192.png" />
+    //     <h3 className="text-3xl font-bold text-white mb-4">Senior Citizen</h3>
+    //     <p className="text-white text-sm max-w-md">
+    //       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    //     </p>
+    //   </div>
+
+    //   {/* RIGHT SIDE */}
+    //   <div className="flex flex-col justify-center p-12 lg:p-20">
+    //     <h2 className="text-3xl font-bold text-gray-800 mb-6">
+    //       Log in to your Account
+    //     </h2>
+
+    //     <form onSubmit={handleLogin} className="space-y-4">
+    //       <div>
+    //         <label className="block text-gray-700 font-medium">Email</label>
+    //         <input
+    //           type="email"
+    //           name="email"
+    //           placeholder="example@email.com"
+    //           className="w-full border p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+    //         />
+    //         <p className="text-xs text-red-500">{errors.email}</p>
+    //       </div>
+
+    //       <div>
+    //         <label className="block text-gray-700 font-medium">Password</label>
+    //         <input
+    //           type="password"
+    //           name="password"
+    //           placeholder="Password"
+    //           className="w-full border p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+    //         />
+    //         <div className="flex justify-between">
+    //           <p className="text-xs text-red-500">{errors.password}</p>
+    //           <Link
+    //             to="/user-reset-password"
+    //             className="text-xs text-red-600 hover:underline"
+    //           >
+    //             Forgot Password?
+    //           </Link>
+    //         </div>
+    //       </div>
+
+    //       <button
+    //         type="submit"
+    //         className="mt-8 w-full py-3 rounded bg-gradient-to-r from-cyan-700 to-blue-700 text-white font-semibold hover:scale-105 transition"
+    //       >
+    //         Login
+    //       </button>
+    //     </form>
+
+    //     <div className="text-center my-6 text-sm text-gray-600">
+    //       Don&apos;t have an account yet?{" "}
+    //       <Link
+    //         to="/user-register"
+    //         className="text-blue-600 font-semibold hover:underline"
+    //       >
+    //         Create an Account
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </div>
+
+    // ---------------------------
+    // RED LOGIN
+    // ---------------------------
     <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen bg-white">
       {/* LEFT SIDE */}
-      <div className="bg-gradient-to-b from-cyan-700 to-blue-700 flex flex-col justify-center items-center text-center p-10 border-8 border-white rounded-2xl">
-        <img src="logo192.png"/>
-        <h3 className="text-3xl font-bold text-white mb-4">Senior Citizen</h3>
-        <p className="text-white text-sm max-w-md">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
+      <div className="bg-gradient-to-b from-[#FF2424] to-[#8B1E2F] flex flex-col justify-center items-center text-center p-10 border-8 border-white rounded-2xl">
+        <div className="flex md:flex-col items-center gap-3">
+          {/* Avatar */}
+          <div className="flex justify-center sm:justify-start">
+            <div className="size-20 md:size-44 rounded-full bg-white flex items-center justify-center shadow-lg filter drop-shadow-[0_0_0.25rem_#242424]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-20 md:size-40 text-red-600"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.963 2.286a.75.75 0 0 0-1.071-.136 9.742 9.742 0 0 0-3.539 6.176 7.547 7.547 0 0 1-1.705-1.715.75.75 0 0 0-1.152-.082A9 9 0 1 0 15.68 4.534a7.46 7.46 0 0 1-2.717-2.248ZM15.75 14.25a3.75 3.75 0 1 1-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 0 1 1.925-3.546 3.75 3.75 0 0 1 3.255 3.718Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+
+          {/* TEXT */}
+          <div>
+            <h3 className="text-2xl md:text-3xl font-bold text-white md:mb-4">
+              San Juan, Metro Manila
+            </h3>
+
+            <p className="text-white text-xs md:text-sm max-w-md">
+              Birthday Cash Gift System for Senior Citizens
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* RIGHT SIDE */}
-      <div className="flex flex-col justify-center p-12 lg:p-20">
+      <div className="flex flex-col justify-center p-8 md:p-12 lg:p-20">
         <h2 className="text-3xl font-bold text-gray-800 mb-6">
           Log in to your Account
         </h2>
@@ -129,7 +227,7 @@ export default function UserLogin() {
 
           <button
             type="submit"
-            className="mt-8 w-full py-3 rounded bg-gradient-to-r from-cyan-700 to-blue-700 text-white font-semibold hover:scale-105 transition"
+            className="mt-8 w-full py-3 rounded bg-gradient-to-l from-[#FF2424] to-[#8B1E2F] text-white font-semibold hover:scale-105 transition"
           >
             Login
           </button>
@@ -139,7 +237,7 @@ export default function UserLogin() {
           Don&apos;t have an account yet?{" "}
           <Link
             to="/user-register"
-            className="text-blue-600 font-semibold hover:underline"
+            className="text-[#FF2424] font-bold hover:underline"
           >
             Create an Account
           </Link>
